38f72fd0ea2b53f87a248d928862938c3f510d8f
steven615 <stevenwang615@gmail.com>
2021-08-19 12:04:32 +0300

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,17 @@ $(document).ready(function () {
   $("#menu").click(function () {
     $("header").toggleClass("opened");
   });
+
+  function toggleMute() {
+    var video = document.getElementById("video");
+    if (video.muted) {
+      video.muted = false;
+    } else {
+      debugger;
+      video.muted = true;
+      video.play();
+    }
+  }
+
+  setTimeout(toggleMute, 1000);
 });
